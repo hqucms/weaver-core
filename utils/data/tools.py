99d6aa56f785a96f2aec6f0108051bd2997a4ca2
Huilin Qu <huilin.qu@cern.ch>
2020-06-03 20:52:25 +0200

    Add utility functions to eval_expr.

--- utils/data/tools.py
+++ utils/data/tools.py
@@ -37,11 +37,11 @@ def _clip(a, a_min, a_max):
 def _get_variable_names(expr, exclude=['awkward', 'np', 'numpy', 'math']):
     import ast
     root = ast.parse(expr)
-    return sorted({node.id for node in ast.walk(root) if isinstance(node, ast.Name)} - set(exclude))
+    return sorted({node.id for node in ast.walk(root) if isinstance(node, ast.Name) and not node.id.startswith('_')} - set(exclude))
 
 
 def _eval_expr(expr, table):
     tmp = {k:table[k] for k in _get_variable_names(expr)}
-    tmp.update({'math':math, 'np':np, 'awkward':awkward})
+    tmp.update({'math': math, 'np': np, 'awkward': awkward, '_concat': _concat, '_pad': _pad, '_clip':_clip})
     return eval(expr, tmp)
 
