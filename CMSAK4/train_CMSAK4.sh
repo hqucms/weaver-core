e06d72e491cee7fa9ad0ffbcf64e042e03199e4d
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-02-21 20:23:34 +0100

    add aux_tot

--- CMSAK4/train_CMSAK4.sh
+++ CMSAK4/train_CMSAK4.sh
@@ -63,6 +63,9 @@ elif [[ "$model" == "PNXT_ef_aux_bin" ]]; then
 elif [[ "$model" == "PNXT_ef_aux" ]]; then
     modelopts="networks/CMSAK4_PNXT_ef.py"
     batchopts="--batch-size 512 --start-lr 1e-2"
+elif [[ "$model" == "PNXT_ef_aux_tot" ]]; then
+    modelopts="networks/CMSAK4_PNXT_ef.py"
+    batchopts="--batch-size 512 --start-lr 1e-2"
 else
     echo "Invalid model $model!"
     exit 1
