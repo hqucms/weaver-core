5de5543695e643922cdaee13a13cf18fdfd463c0
Huilin Qu <huilin.qu@cern.ch>
2024-08-07 17:37:22 +0200

    Take extra-selection into account when making wgt.

--- weaver/utils/data/preprocess.py
+++ weaver/utils/data/preprocess.py
@@ -7,6 +7,7 @@ import awkward as ak
 from ..logger import _logger, warn_n_times
 from .eval_utils import _get_variable_names, _eval_expr
 from .fileio import _read_files
+from .config import _strcat
 
 
 def _apply_selection(table, selection, funcs=None):
@@ -79,12 +80,13 @@ class AutoStandardizer(object):
         data_config (DataConfig): object containing data format information.
     """
 
-    def __init__(self, filelist, data_config):
+    def __init__(self, filelist, data_config, extra_selection=None):
         if isinstance(filelist, dict):
             filelist = sum(filelist.values(), [])
         self._filelist = filelist if isinstance(
             filelist, (list, tuple)) else glob.glob(filelist)
         self._data_config = data_config.copy()
+        self._data_config.selection = _strcat(self._data_config.selection, extra_selection)
         self.load_range = (0, data_config.preprocess.get('data_fraction', 0.1))
 
     def read_file(self, filelist):
@@ -163,11 +165,12 @@ class WeightMaker(object):
         data_config (DataConfig): object containing data format information.
     """
 
-    def __init__(self, filelist, data_config):
+    def __init__(self, filelist, data_config, extra_selection=None):
         if isinstance(filelist, dict):
             filelist = sum(filelist.values(), [])
         self._filelist = filelist if isinstance(filelist, (list, tuple)) else glob.glob(filelist)
         self._data_config = data_config.copy()
+        self._data_config.selection = _strcat(self._data_config.selection, extra_selection)
 
     def read_file(self, filelist):
         keep_branches = set(self._data_config.reweight_branches + self._data_config.reweight_classes)
@@ -188,6 +191,7 @@ class WeightMaker(object):
         _logger.debug('[WeightMaker] keep_branches:\n  %s', ','.join(keep_branches))
         _logger.debug('[WeightMaker] aux_branches:\n  %s', ','.join(aux_branches))
         _logger.debug('[WeightMaker] load_branches:\n  %s', ','.join(load_branches))
+        _logger.info('[WeightMaker] selection: %s', self._data_config.selection)
 
         table = _read_files(filelist, load_branches, show_progressbar=True,
                             treename=self._data_config.treename,
